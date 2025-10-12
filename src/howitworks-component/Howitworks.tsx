5dd30635b8f16de31494a28cbc67d9da09c078f8
Rojan Dhungel <rojandhungel11@gmail.com>
2025-10-12 11:28:18 +0545

    structrue changed

--- src/howitworks-component/Howitworks.tsx
+++ src/howitworks-component/Howitworks.tsx
@@ -5,7 +5,7 @@ import FAQSection from "@/howitworks-component/FAQSection"
 import HowItWorksHeroSection from "@/howitworks-component/HowItWorksHeroSection"
 import ServicesSection from "@/howitworks-component/ServicesSection"
 import StepsSection from "@/howitworks-component/StepsSection"
-import Footer from "@/pages/Footer"
+import Footer from "@/layout/Footer"
 
 const Howitworks = () => {
   return (
