0ef478a4d0b31c5b20a4a326abc456aac204fb67
Rojan Dhungel <rojandhungel11@gmail.com>
2025-10-17 15:24:59 +0545

    toc completed

---
+++ src/pages/terms.tsx
@@ -0,0 +1,12 @@
+import TermsAndConditions from '@/Documents/TermsandConditions'
+import React from 'react'
+
+const terms = () => {
+  return (
+    <>
+      <TermsAndConditions />
+    </>
+  )
+}
+
+export default terms
